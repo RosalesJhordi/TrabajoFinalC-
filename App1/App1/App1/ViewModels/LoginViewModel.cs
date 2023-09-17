e15c886d5a9a4dc5cf1386b0389e9257fa988dee
RosalesJhon <yhordiyhom65@gmail.com>
2023-09-16 19:40:56 -0500

    Login - xamarin parte2

--- App1/App1/App1/ViewModels/LoginViewModel.cs
+++ App1/App1/App1/ViewModels/LoginViewModel.cs
@@ -8,37 +8,26 @@ namespace App1.ViewModels
 {
 	public class LoginViewModel : BaseViewModel
 	{
-
-		private string username;
-		private string password;
-
-		public string Username
-		{
-			get { return username; }
-			set { SetProperty(ref username, value); }
-		}
-
-		public string Password
-		{
-			get { return password; }
-			set { SetProperty(ref password, value); }
-		}
-
-		public ICommand LoginCommand { get; }
-
-		public LoginViewModel()
-		{
-			LoginCommand = new Command(ExecuteLogin);
-		}
-
 		private void ExecuteLogin()
 		{
-			// Aquí obtienes los valores de Username y Password y haces lo que necesites con ellos
-			string enteredUsername = Username;
-			string enteredPassword = Password;
-
 			// Por ejemplo, puedes mostrarlos en la consola de depuración
-			System.Diagnostics.Debug.WriteLine($"Username: {enteredUsername}, Password: {enteredPassword}");
+			System.Diagnostics.Debug.WriteLine($"Username:");
+		}
+		//control para mostrar contraseña
+		private bool isPwdVisible;
+		public bool IsPwdVisible
+		{
+			get { return isPwdVisible; }
+			set
+			{
+				isPwdVisible = value;
+				OnPropertyChanged(nameof(IsPwdVisible));
+			}
+		}
+
+		private void OnCheckBoxCheckedChanged(bool isChecked)
+		{
+			IsPwdVisible = isChecked; // contraseña visible
 		}
 	}
 }
--- App1/App1/App1/obj/Debug/netstandard2.0/Views/LoginPage.xaml.g.cs
+++ App1/App1/App1/obj/Debug/netstandard2.0/Views/LoginPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace App1.Views {
     public partial class LoginPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.CheckBox myCheckBox;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(LoginPage));
+            myCheckBox = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CheckBox>(this, "myCheckBox");
         }
     }
 }
